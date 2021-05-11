bcfa449530904367f9e9c778c2c718dda273998a
Calderonio_Giuseppe <10630087@polimi.it>
2021-05-11 23:32:13 +0200

    This commit adds the class relative for remote connections

---
+++ src/main/java/it/polimi/ingsw/network/client/ClientMainRemote.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.network.client;
+
+import it.polimi.ingsw.network.Client;
+
+/**
+ * this class represent the main
+ */
+public class ClientMainRemote {
+
+    /**
+     * this method start the game, calling the constructor of the client with the
+     * IP address of the server and the port
+     * @param args these are the arguments of the program.
+     *             in particular, the fist argument contain the port of the server,
+     *             and the second argument contain the IP address of the server
+     */
+    public static void main(String[] args) {
+
+        new Client(args[1], Integer.parseInt(args[0]));
+    }
+}
